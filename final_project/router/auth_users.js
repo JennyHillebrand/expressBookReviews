20e7349dc8fcbd5f3c95e8e1ac73f472c1ff85ea
JennyHillebrand <jenny.hillebrand@gmail.com>
2022-12-09 05:37:23 -0500

    actually after task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -46,8 +46,21 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=req.params.isbn;
+  const review=req.query.review;
+  const username=req.user;
+  if (!(isbn in books)){
+      return res.status(300).json({message: "No such ISBN"})
+  }
+  else {
+      books[isbn]["reviews"][username]=review
+      return res.status(200).json({message:"Review added successfully " + isbn +" " + review})
+  }
+
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
+//regd_users.delete("/auth/review/:isbn", (req, res) => {
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
